e072b4b4d6cafe87d39d0dae7c3913ed2c4a4ab0
Romain THERRAT <romain42@gmail.com>
2018-02-07 11:09:55 +0100

    Add ability to remove sftp account

--- manifests/sftp_chroot.pp
+++ manifests/sftp_chroot.pp
@@ -3,6 +3,7 @@
 #
 define openssh::sftp_chroot(
   $chroot_directory,
+  $ensure               = 'present',
   $login                = $name,
   $group                = $name,
   $type                 = 'user',
@@ -11,16 +12,26 @@ define openssh::sftp_chroot(
   $command              = 'internal-sftp',
   $template             = 'openssh/sftp_chroot.erb'
 ){
-  
+
   include openssh
 
   $bool_allow_tcp_forwarding = any2bool($allow_tcp_forwarding)
   $bool_x11_forwarding = any2bool($x11_forwarding)
 
+  $manage_ensure_present_or_absent = $ensure ? {
+    'absent' => 'absent',
+    default  => 'present',
+  }
+  $manage_ensure_directory_or_absent = $ensure ? {
+    'absent' => 'absent',
+    default  => 'directory',
+  }
+
   if $openssh::use_concat == false {
     fail("In order to user openssh::sftp_chroot you have to enable [*use_concat*].")
   } else {
     concat::fragment{"openssh-sftp-chroot-$name":
+      ensure  => $manage_ensure_present_or_absent,
       target  => $openssh::config_file,
       content => template($template),
       order   => '50',
@@ -28,7 +39,7 @@ define openssh::sftp_chroot(
 
     file { "openssh-sftp-chroot-$name":
       path   => $chroot_directory,
-      ensure => directory,
+      ensure => $manage_ensure_directory_or_absent,
       owner  => 'root',
       group  => 'root',
     }
